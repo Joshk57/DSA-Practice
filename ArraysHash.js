c7fbba419b0c484f677b4f4c7e49ad31010bef45
joshua <joshk.5793@gmail.com>
2024-01-28 19:09:26 -0500

    finished contains duplicates

--- ArraysHash.js
+++ ArraysHash.js
@@ -1,12 +1,21 @@
 // 1. Contains Duplicates
 
 var containsDuplicate = function(nums) {
-    
+    let hash = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = 1
+        } else {
+            return true
+        }
+    }
+    return false
 };
 
 
 
-containsDuplicate(nums)
-nums = [1,2,3,1]
-nums = [1,2,3,4]
-nums = [1,1,1,3,3,4,3,2,4,2]
+// nums = [1,2,3,1]
+// nums = [1,2,3,4]
+// nums = [1,1,1,3,3,4,3,2,4,2]
+// console.log(containsDuplicate(nums))
