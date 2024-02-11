8d6c73b2b8c908e8600898db22de404e7ec5ac9e
root <root@DESKTOP-VAR3S3L>
2024-02-10 22:37:09 -0500

    isPalindrome

---
+++ TwoPointers.js
@@ -0,0 +1,15 @@
+var isPalindrome = function(s) {
+    
+    const chars = s.split(" ")
+
+    console.log(chars)
+    return chars
+    // for (let i = 0; i < chars.length; i++) {
+        
+    // }
+};
+
+s = "A man, a plan, a canal: Panama"
+// s = "race a car"
+// s = " "
+console.log(isPalindrome(s))
