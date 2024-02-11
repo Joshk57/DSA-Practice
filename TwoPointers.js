c50a65fac100154adbb5f485b277ee0658cdffac
root <root@DESKTOP-VAR3S3L>
2024-02-10 22:47:55 -0500

    finished twoSum

--- TwoPointers.js
+++ TwoPointers.js
@@ -2,14 +2,31 @@ var isPalindrome = function(s) {
     
     const chars = s.split(" ")
 
-    console.log(chars)
-    return chars
-    // for (let i = 0; i < chars.length; i++) {
+
+    for (let i = 0; i < chars.length; i++) {
         
-    // }
+    }
 };
 
-s = "A man, a plan, a canal: Panama"
+// s = "A man, a plan, a canal: Panama"
 // s = "race a car"
 // s = " "
-console.log(isPalindrome(s))
+// console.log(isPalindrome(s))
+
+
+var twoSum = function(numbers, target) {
+    
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                return [i + 1, j + 1]
+            }
+        }
+    }
+};
+
+numbers = [2,7,11,15], target = 9
+numbers = [2,3,4], target = 6
+numbers = [-1,0], target = -1
+console.log(twoSum(numbers, target))
