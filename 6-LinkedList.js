8220b2ac704cd69f286dd7bcd4ae2c38b2367a5a
Joshua Kim <joshk.5793@gmail.com>
2024-02-17 22:14:35 -0500

    attempting merge two lists

--- 6-LinkedList.js
+++ 6-LinkedList.js
@@ -12,5 +12,34 @@ var reverseList = function(head) {
 
 // head = [1,2,3,4,5]
 // head = [1,2]
-head = []
-console.log(reverseList(head))
+// head = []
+// console.log(reverseList(head))
+
+
+
+// Merge two sorted lists
+
+var mergeTwoLists = function(list1, list2) {
+    
+
+    let newArr = []
+
+    for (let i = 0; i < list1.length; i++) {
+        for (let j = 0; j < list2.length; j++) {
+            if (list1[i] > list2[j]) {
+                newArr.push(list2[j])
+            } else if (list1[i] < list2[j]){
+                newArr.push(list1[i])
+            } else {
+                newArr.push(list1[i], list2[j])
+            }
+        }
+    }
+    return newArr
+};
+
+
+list1 = [1,2,4], list2 = [1,3,4]
+// list1 = [], list2 = []
+// list1 = [], list2 = [0]
+console.log(mergeTwoLists(list1, list2))
