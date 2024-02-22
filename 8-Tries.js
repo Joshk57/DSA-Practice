9643b000c10d45e160aa07a838bab863ee1cc724
Joshua Kim <joshk.5793@gmail.com>
2024-02-22 15:42:28 -0500

    starting Tries

---
+++ 8-Tries.js
@@ -0,0 +1,35 @@
+
+var Trie = function() {
+    
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    
+};
+
+
+
+
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+
