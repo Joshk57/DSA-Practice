158743f28eb0fd816a733a6cba88e34f981dfdcc
Joshua Kim <joshk.5793@gmail.com>
2024-02-24 14:48:58 -0500

    daily temp stacak

--- 3-Stack.js
+++ 3-Stack.js
@@ -15,7 +15,7 @@ var isValid = function(s) {
 s = "()"
 // s = "()[]{}"
 // s = "(]"
-console.log(isValid(s))
+// console.log(isValid(s))
 
 
 var MinStack = function() {
@@ -59,3 +59,61 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+
+
+
+// Evaluate Reverse Polish Notation
+
+var evalRPN = function(tokens) {
+    
+};
+
+tokens = ["2","1","+","3","*"]
+// tokens = ["4","13","5","/","+"]
+// tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// console.log(evalRPN(tokens))
+
+
+// Generate Paranthesis
+
+var generateParenthesis = function(n) {
+    if (n === 1) {
+        return ["()"]
+    }
+
+
+};
+
+n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+n = 1
+// Output: ["()"]
+
+// console.log(generateParenthesis(n))
+
+
+//Daily Temperatures
+
+var dailyTemperatures = function(temperatures) {
+    
+    let temps = []
+    for (let i = 0; i < temperatures.length; i++) {
+        let count = 0
+        if (temperatures[i] < temperatures[i+1]) {
+            count++
+            temps.push(count)
+            count = 0
+        } else {
+            count++
+ 
+        }
+    }
+    return temps
+};
+
+temperatures = [73,74,75,71,69,72,76,73]
+// temperatures = [30,40,50,60]
+// temperatures = [30,60,90]
+console.log(dailyTemperatures(temperatures))
