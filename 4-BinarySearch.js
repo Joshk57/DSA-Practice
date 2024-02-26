bbecac493f9b24b423afe1942727fc42128ed0d2
Joshua Kim <joshk.5793@gmail.com>
2024-02-26 11:30:02 -0500

    search matrix

--- 4-BinarySearch.js
+++ 4-BinarySearch.js
@@ -14,5 +14,26 @@ var search = function(nums, target) {
 
 
 // nums = [-1,0,3,5,9,12], target = 9
-nums = [-1,0,3,5,9,12], target = 2
-console.log(search(nums, target))
+// nums = [-1,0,3,5,9,12], target = 2
+// console.log(search(nums, target))
+
+
+// Search a 2D Matrix
+
+var searchMatrix = function(matrix, target) {
+    
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === target) {
+                return true
+            }
+        }
+    }
+    return false
+};
+
+
+// matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// console.log(searchMatrix(matrix, target))
