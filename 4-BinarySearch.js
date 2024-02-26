a3ba449f0fadb01febc4e9d0c39d147190b43897
Joshua Kim <joshk.5793@gmail.com>
2024-02-26 11:38:36 -0500

    koko eating bananas

--- 4-BinarySearch.js
+++ 4-BinarySearch.js
@@ -37,3 +37,31 @@ var searchMatrix = function(matrix, target) {
 // matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 // matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // console.log(searchMatrix(matrix, target))
+
+
+// Koko Eating Bananas
+
+var minEatingSpeed = function(piles, h) {
+    let lo = 1, hi = Math.max(...piles), k = -1;
+    
+    while (lo <= hi) {
+        const mid = lo + ((hi - lo) >> 1);
+        const numOfHours = piles.reduce((total, pile) => total + Math.ceil(pile / mid), 0);
+        
+        if (numOfHours <= h) {
+            k = mid;
+            hi = mid - 1;
+        }
+        else {
+            lo = mid + 1;
+        }
+    }
+    
+    return k;
+};
+
+piles = [3,6,7,11], h = 8
+// piles = [30,11,23,4,20], h = 5
+// piles = [30,11,23,4,20], h = 6
+
+console.log(minEatingSpeed(piles, h))
