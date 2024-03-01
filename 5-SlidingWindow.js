113da8196c6039a2325120561a84f377dcdfe259
Joshua Kim <joshk.5793@gmail.com>
2024-03-01 18:41:30 -0500

    length of longest substring

--- 5-SlidingWindow.js
+++ 5-SlidingWindow.js
@@ -18,4 +18,26 @@ var maxProfit = function(prices) {
 
 prices = [7,1,5,3,6,4]
 // prices = [7,6,4,3,1]
-console.log(maxProfit(prices))
+// console.log(maxProfit(prices))
+
+// Longest Substring Without Repeating Characters
+
+var lengthOfLongestSubstring = function(s) {
+  const lastSeen = {};
+  let longest = [0,1];
+  let startIdx = 0;
+ 
+  for(let i=0;i<s.length;i++){
+    const char = s[i];
+    if( char in lastSeen)  startIdx = Math.max(startIdx, lastSeen[char] + 1);
+    if(longest[1]-longest[0] < i+ 1 - startIdx) longest = [startIdx,i+1];
+     lastSeen[char] = i;
+  }
+   return s.slice(longest[0],longest[1]).length;
+};
+
+s = "abcabcbb"
+// s = "bbbbb"
+// s = "pwwkew"
+
+console.log(lengthOfLongestSubstring(s))
