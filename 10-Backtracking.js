e4604ff712bc4e39fa412c7812a0822c723cd72d
Joshua Kim <joshk.5793@gmail.com>
2024-03-08 19:45:04 -0500

    combination sum

--- 10-Backtracking.js
+++ 10-Backtracking.js
@@ -21,3 +21,24 @@ var subsets = function(nums) {
 nums = [1,2,3]
 // nums = [0]
 console.log(subsets(nums))
+
+//Combination Sum
+
+var combinationSum = function(candidates, target) {
+    let res = [];
+
+    const dfs = (curCandidates, curTarget, value) => {
+        if( curTarget == 0)  res.push(value);
+        if(curTarget <= 0 ) return;
+        for(let g=0; g<curCandidates.length; g++){
+            dfs(curCandidates.slice(g), curTarget-curCandidates[g], [...value, curCandidates[g] ] );  
+        }
+    }
+    dfs(candidates, target, []);
+    return res;
+};
+
+candidates = [2,3,6,7], target = 7
+// candidates = [2,3,5], target = 8
+// candidates = [2], target = 1
+console.log(combinationSum(candidates, target))
