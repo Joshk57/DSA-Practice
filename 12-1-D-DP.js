5d245d6da053ebde59d04a6ba90d553d45de2048
Joshua Kim <joshk.5793@gmail.com>
2024-03-15 15:32:08 -0400

    attempting climbStairs

---
+++ 12-1-D-DP.js
@@ -0,0 +1,32 @@
+// Climbing Stairs
+
+var climbStairs = function(n) {
+    
+    let count = 1
+
+    if (n % 2 === 0 )
+};
+
+n = 2
+n = 3
+
+console.log(climbStairs(n))
+
+4
+
+1 + 1 + 1 + 1
+1 + 2 + 1
+2 + 1 + 1
+1 + 1 + 2
+2 + 2
+
+5
+
+1 1 1 1 1
+1 2 1 1
+1 1 2 1
+1 1 1 2 
+2 1 1 1
+1 2 2
+2 1 2
+2 2 1
