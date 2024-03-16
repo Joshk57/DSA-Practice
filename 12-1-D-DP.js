9d0ba12180492619525672e84bdfc9c5bd7e5d89
Joshua Kim <joshk.5793@gmail.com>
2024-03-16 17:47:46 -0400

    changes to rob function

--- 12-1-D-DP.js
+++ 12-1-D-DP.js
@@ -66,20 +66,34 @@ cost = [10,15,20]
 
 var rob = function(nums) {
     
-    let sum = 0
+    let oddSum = 0
+    let evenSum = 0
 
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         let num = nums[i] + nums[j]
+    //         if (i - j !== (-1 || 1) && (num > sum)) {
+    //             sum = num
+    //         }
+    //     } 
+    // }
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            let num = nums[i] + nums[j]
-            if (i - j !== (-1 || 1) && (num > sum)) {
-                sum = num
-            }
-        } 
+        if (i % 2 !== 0) {
+            oddSum += nums[i]
+        } else {
+            evenSum += nums[i]
+        }
     }
-    return sum
+
+    if (oddSum >= evenSum) {
+        return oddSum
+    } else {
+        return evenSum
+    }
+
 };
 
-// nums = [1,2,3,1]
-nums = [2,7,9,3,1]
+nums = [1,2,3,1]
+// nums = [2,7,9,3,1]
 console.log(rob(nums))
