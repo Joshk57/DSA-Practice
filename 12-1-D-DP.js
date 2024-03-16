b420c46e406ec9cc58fdbd1c977ff836768144fa
Joshua Kim <joshk.5793@gmail.com>
2024-03-16 17:41:27 -0400

    working on rob function

--- 12-1-D-DP.js
+++ 12-1-D-DP.js
@@ -57,4 +57,29 @@ var minCostClimbingStairs = function(cost) {
 cost = [10,15,20]
 // cost = [1,100,1,1,1,100,1,1,100,1]
 
-console.log(minCostClimbingStairs(cost))
+// console.log(minCostClimbingStairs(cost))
+
+
+
+
+// House Robber
+
+var rob = function(nums) {
+    
+    let sum = 0
+
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let num = nums[i] + nums[j]
+            if (i - j !== (-1 || 1) && (num > sum)) {
+                sum = num
+            }
+        } 
+    }
+    return sum
+};
+
+// nums = [1,2,3,1]
+nums = [2,7,9,3,1]
+console.log(rob(nums))
