9e381e63a5871472c6ac3e393c6368d92a655981
Joshua Kim <joshk.5793@gmail.com>
2024-03-23 17:14:41 -0400

    longest repeating character replacement

--- 4-BinarySearch.js
+++ 4-BinarySearch.js
@@ -102,3 +102,4 @@ nums = [4,5,6,7,0,1,2], target = 0
 // nums = [4,5,6,7,0,1,2], target = 3
 // nums = [1], target = 0
 console.log(search(nums, target))
+
--- 5-SlidingWindow.js
+++ 5-SlidingWindow.js
@@ -41,3 +41,32 @@ s = "abcabcbb"
 // s = "pwwkew"
 
 console.log(lengthOfLongestSubstring(s))
+
+// Longest Repeating Character Replacement
+
+var characterReplacement = function(s, k) {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
+
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+
+  return right - left;
+};
+
+s = "ABAB", k = 2
+// s = "AABABBA", k = 1
+console.log(characterReplacement)
