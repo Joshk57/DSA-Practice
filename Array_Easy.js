4cebaeb81764037780dfe77ba0017eceebda834a
Joshua Kim <joshk.5793@gmail.com>
2024-03-29 18:14:41 -0400

    richest customer wealth

--- Array_Easy.js
+++ Array_Easy.js
@@ -14,4 +14,27 @@ var maxWidthOfVerticalArea = function(points) {
 
 points = [[8,7],[9,9],[7,4],[9,7]]
 // points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
-console.log(maxWidthOfVerticalArea(points))
+// console.log(maxWidthOfVerticalArea(points))
+
+
+// 1672 Richest Customer Wealth
+
+var maximumWealth = function(accounts) {
+    
+    let maxWealth = 0
+    for (let i = 0; i < accounts.length; i++) {
+        let currentWealth = 0
+        for (let j = 0; j < accounts[i].length; j++) {
+            currentWealth += accounts[i][j]
+        }
+        if (currentWealth > maxWealth) {
+            maxWealth = currentWealth
+        }
+    }
+    return maxWealth
+};
+
+// accounts = [[1,2,3],[3,2,1]]
+// accounts = [[1,5],[7,3],[3,5]]
+accounts = [[2,8,7],[7,1,3],[1,9,5]]
+console.log(maximumWealth(accounts))
