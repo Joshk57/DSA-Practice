7cf4e4f5b1f870da9055e9d5db275b3266083fbb
Joshua Kim <joshk.5793@gmail.com>
2024-04-06 11:51:56 -0400

    jewels and stones

--- String_Easy.js
+++ String_Easy.js
@@ -16,4 +16,25 @@ var defangIPaddr = function(address) {
 
 address = "1.1.1.1"
 // address = "255.100.50.0"
-console.log(defangIPaddr(address))
+// console.log(defangIPaddr(address))
+
+
+// 771 Jewels and Stones
+
+var numJewelsInStones = function(jewels, stones) {
+    
+    let count = 0
+
+    for (let i = 0; i < stones.length; i++) {
+        for (let j = 0; j < jewels.length; j++) {
+            if (stones[i] === jewels[j]) {
+                count++
+            }
+        }
+    }
+    return count
+};
+
+// jewels = "aA", stones = "aAAbbbb"
+jewels = "z", stones = "ZZ"
+console.log(numJewelsInStones(jewels, stones))
