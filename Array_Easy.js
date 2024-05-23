c5dec078a737a17b5a13457ec666f3951c8d3fcb
Joshua Kim <joshk.5793@gmail.com>
2024-05-23 19:15:48 -0400

    solved 1572

--- Array_Easy.js
+++ Array_Easy.js
@@ -337,7 +337,10 @@ allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
 // 1572. Matrix Diagonal Sum
 
 var diagonalSum = function(mat) {
-    
+    let sum = 0, n = mat.length;
+    for(let i=0; i<n; i++) sum+=mat[i][i];
+    for(let i=0; i<n; i++) if(n%2==0 || i!==n>>1) sum+=mat[n-i-1][i];
+    return sum;    
 };
 
 mat = [[1,2,3],
