907acc1845d8e6deffdbb8fb7e526d189fc6f897
Joshua Kim <joshk.5793@gmail.com>
2024-05-31 23:04:52 -0400

    solved 804

--- Array_Easy.js
+++ Array_Easy.js
@@ -389,7 +389,44 @@ nums = [1,2,3,2]
 // 804. Unique Morse Code Words
 
 var uniqueMorseRepresentations = function(words) {
-    
+    let hashMap = {
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--.."
+      }
+      let currentIndexStr = ''
+      for (let i = 0; i < words.length; i++) {
+        let currentLength = words[i].length
+        for (let j = 0; j < currentLength; j++) {
+          currentIndexStr += hashMap[words[i][j]]
+        }
+        words[i] = currentIndexStr
+        currentIndexStr = ''
+      }
+      return new Set(words).size
 };
 
 
