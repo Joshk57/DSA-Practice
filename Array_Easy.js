9d4077f89553653f957b70a4ee6de134556066bd
Joshua Kim <joshk.5793@gmail.com>
2024-06-06 14:53:17 -0400

    solved 2697

--- Array_Easy.js
+++ Array_Easy.js
@@ -481,7 +481,15 @@ s = "l|*e*et|c**o|*de|"
 // 2697. Lexicographically Smallest Palindrome
 
 var makeSmallestPalindrome = function(s) {
-    
+    let str = s.split('')
+    for(let i = s.length - 1, j = 0; i >= Math.ceil(s.length / 2); i--, j++) {
+        if(str[i] < str[j]) {
+            str[j] = str[i]
+        } else {
+            str[i] = str[j]
+        }
+    }
+    return str.join('')
 };
 
 s = "egcfe"
