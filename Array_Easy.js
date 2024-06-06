bbc729599771c8ea1917b41f43cc446ea82eaab9
Joshua Kim <joshk.5793@gmail.com>
2024-06-06 14:55:58 -0400

    1844 solved

--- Array_Easy.js
+++ Array_Easy.js
@@ -502,6 +502,19 @@ s = "egcfe"
 
 
 var replaceDigits = function(s) {
+    let d = "";
+
+    for (let i of s) {
+  if (!isNaN(i)) {
+    console.log("i is", i);
+    let z = d.charCodeAt(d.length - 1) + parseInt(i);
+    d += String.fromCharCode(z);
+  } else {
+    d += i;
+    console.log("else d is", d);
+  }
+    }
+      return d
     
 };
 
